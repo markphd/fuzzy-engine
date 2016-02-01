e5bb146e5bc5f0e2f7e1a5119eb081dfba6d13ad
markphd <radical.mark@gmail.com>
2016-02-01 22:33:39 +0400

    Added moment module and fixed image and index handlebars path

--- server/configure.js
+++ server/configure.js
@@ -7,6 +7,7 @@ var path = require('path'),
     morgan = require('morgan'),
     methodOverride = require('method-override'),
     errorHandler = require('errorhandler');
+    moment = require('moment');
 
 module.exports = function(app) {
 app.use(morgan('dev'));
@@ -24,7 +25,12 @@ if ('development' === app.get('env')) {
     app.engine('handlebars', exphbs.create({
         defaultLayout: 'main',
         layoutsDir: app.get('views') + '/layouts',
-        partialsDir: [app.get('views') + '/partials']
+        partialsDir: [app.get('views') + '/partials'],
+        helpers: {
+          timeago: function(timestamp) {
+            return moment(timestamp).startOf('minute').fromNow();
+          } 
+        }
     }).engine);
     app.set('view engine', 'handlebars');
     
