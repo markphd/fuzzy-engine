348e3a80cbfc307f42e99c8d984466182230be76
markphd <radical.mark@gmail.com>
2016-02-10 04:23:54 +0400

    randomUser saved using mongoose .save()

--- mongotest/test.js
+++ mongotest/test.js
@@ -1,7 +1,43 @@
+// INSERT DATA TO TESTING COLLECTION
+
+// var MongoClient = require('mongodb').MongoClient;
+
+// MongoClient.connect('mongodb://localhost:27017/mongotest', function(err, db) {
+//     console.log('Connected to MongoDB!');
+
+//     var collection = db.collection('testing');
+
+//     collection.insert({'title': 'Snowcrash'}, function(err, docs) {
+// 	    // on successful insertion, log to the screen the new
+// 	    // collection's details:
+// 	    console.log(docs.length + ' record inserted.');
+// 	    console.log(docs[0].title + ' – ' + docs[0]._id);
+
+// 	    // finally close the connection:
+// 	    db.close();
+//     });
+
+// });
+
+// RETRIEVE DATA FROM TESTING COLLECTION
+
 var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/mongotest', function(err, db) {
     console.log('Connected to MongoDB!');
 
-    db.close();
+    var collection = db.collection('testing');
+    collection.insert({
+        'title': 'Snowcrash'
+    }, function(err, docs) {
+        console.log(collection.length + ' record inserted.');
+        console.log(collection[0]._id + ' - ' + collection[0].title);
+
+        collection.findOne({
+            title: 'Snowcrash'
+        }, function(err, doc) {
+            console.log(doc._id + ' - ' + doc.title);
+            db.close();
+        });
+    });
 });
