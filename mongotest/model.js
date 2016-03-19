1719055ca2959bf247f4aa981224f677b3f62228
markphd <radical.mark@gmail.com>
2016-03-19 07:56:20 +0400

    add logo to layout, updated styles.css, implemented findByAgeRange()

--- mongotest/model.js
+++ mongotest/model.js
@@ -14,19 +14,18 @@ active: { type: Boolean, default: false },
 age: { type: Number, required: true, min: 13, max: 120 }
 });
 
-// Account.statics.findByAgeRange = function(min, max, callback) {
-// this.find({ age: { $gt : min, $lte : max} }, callback);
-// };
+Account.statics.findByAgeRange = function(min, max, callback) {
+this.find({ age: { $gt : min, $lte : max} }, callback);
+};
 
 var AccountModel = mongoose.model('Account', Account);
-var invalidUser = new AccountModel({  username: 'invalid', active: true, age: 11 });
-invalidUser.validate(function(err) {
-console.log(err);
-});
+// var invalidUser = new AccountModel({  username: 'invalid', active: true, age: 11 });
+// invalidUser.validate(function(err) {
+// console.log(err);
+// });
 
 // invalidUser.save();
 
-
-// AccountModel.findByAgeRange(18, 30, function(err, accounts){
-// console.log(accounts.length);    // => 2
-// });
+AccountModel.findByAgeRange(18, 31, function(err, accounts){
+console.log(accounts.length);    // => 2
+});
